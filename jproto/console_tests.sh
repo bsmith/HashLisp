bdbe9fa08441d4c45a874694feac49800e6897a8
Benjamin Smith <bsmith@cabbage.org.uk>
2023-06-13 14:27:38 +0100

    Run tests from the console with gradlew

--- jproto/console_tests.sh
+++ jproto/console_tests.sh
@@ -6,7 +6,7 @@ mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:get -Dartifact=org.juni
 
 mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:copy -Dartifact=org.junit.platform:junit-platform-console-standalone:1.9.3 -DoutputDirectory=.
 
-gradle classes testClasses
+./gradlew classes testClasses
 
 # XXX: default to --scan-classpath or something if no args provided
 exec java -jar junit-platform-console-standalone-1.9.3.jar -cp build/classes/java/main -cp build/classes/java/test "$@"
