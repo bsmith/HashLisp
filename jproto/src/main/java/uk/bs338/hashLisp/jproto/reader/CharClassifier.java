580496b38c0933318b715b18cae2633399b59c5d
Benjamin Smith <bsmith@cabbage.org.uk>
2023-06-19 20:40:31 +0100

    CharClassifier: use a table to store the class of every character

--- jproto/src/main/java/uk/bs338/hashLisp/jproto/reader/CharClassifier.java
+++ jproto/src/main/java/uk/bs338/hashLisp/jproto/reader/CharClassifier.java
@@ -1,12 +1,9 @@
 package uk.bs338.hashLisp.jproto.reader;
 
+import java.util.ArrayList;
 import java.util.EnumSet;
 
 public class CharClassifier {
-    private final static String symbolChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-?!$@";
-    private final static String digits = "0123456789";
-    private final static String whitespace = " \t\r\n";
-    
     enum CharClass {
         OPEN_PARENS,
         CLOSE_PARENS,
@@ -18,33 +15,42 @@ public class CharClassifier {
         WHITESPACE
     }
     
-    /* XXX rewrite this so that this is the more efficient impl! */
-    public EnumSet<CharClass> classifyChar(int codepoint) {
-        return classifyChar(new String(new int[]{codepoint}, 0, 1));
+    private final static String symbolChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-?!$@";
+    private final static String digits = "0123456789";
+    private final static String whitespace = " \t\r\n";
+    
+    private final static int TABLE_SIZE = 256;
+    private static ArrayList<EnumSet<CharClass>> classTable = null;
+    
+    public CharClassifier() {
+        setupTables();
     }
     
-    /* public EnumSet<CharClass> classifyChar(String ch) {
-           return classifyChar(ch.codepointAt(0));
-       } */
-
-    public EnumSet<CharClass> classifyChar(String ch)  {
-        var set = EnumSet.noneOf(CharClass.class);
-        if (ch.startsWith("("))
-            set.add(CharClass.OPEN_PARENS);
-        if (ch.startsWith(")"))
-            set.add(CharClass.CLOSE_PARENS);
-        if (symbolChars.contains(ch.substring(0, 1)))
-            set.add(CharClass.SYMBOL_CHAR);
-        if (digits.contains(ch.substring(0, 1)))
-            set.add(CharClass.DIGIT_CHAR);
-        if (ch.startsWith("#"))
-            set.add(CharClass.HASH_CHAR);
-        if (ch.startsWith(":"))
-            set.add(CharClass.COLON_CHAR);
-        if (ch.startsWith("."))
-            set.add(CharClass.DOT_CHAR);
-        if (whitespace.contains(ch.substring(0, 1)))
-            set.add(CharClass.WHITESPACE);
-        return set;
+    private void setupTables() {
+        if (classTable != null)
+            return;
+        classTable = new ArrayList<>(TABLE_SIZE);
+        for (int cp = 0; cp < TABLE_SIZE; cp++)
+            classTable.add(cp, EnumSet.noneOf(CharClass.class));
+        
+        classTable.get('(').add(CharClass.OPEN_PARENS);
+        classTable.get(')').add(CharClass.CLOSE_PARENS);
+        classTable.get('#').add(CharClass.HASH_CHAR);
+        classTable.get(':').add(CharClass.COLON_CHAR);
+        classTable.get('.').add(CharClass.DOT_CHAR);
+        symbolChars.codePoints().forEach(cp -> classTable.get(cp).add(CharClass.SYMBOL_CHAR));
+        digits.codePoints().forEach(cp -> classTable.get(cp).add(CharClass.DIGIT_CHAR));
+        whitespace.codePoints().forEach(cp -> classTable.get(cp).add(CharClass.WHITESPACE));
+    }
+    
+    /* XXX rewrite this so that this is the more efficient impl! */
+    public EnumSet<CharClass> classifyChar(int codepoint) {
+        if (codepoint < TABLE_SIZE)
+            return EnumSet.copyOf(classTable.get(codepoint));
+        return EnumSet.noneOf(CharClass.class);
+    }
+    
+    public EnumSet<CharClass> classifyChar(String ch) {
+        return classifyChar(ch.codePointAt(0));
     }
 }
