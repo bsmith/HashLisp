48bf1eeeb057c1af78cd2106ae243b4ba31c6014
Benjamin Smith <bsmith@cabbage.org.uk>
2023-07-10 21:26:34 +0100

    Better default impl of eval_multi

--- jproto/src/main/java/uk/bs338/hashLisp/jproto/IEvaluator.java
+++ jproto/src/main/java/uk/bs338/hashLisp/jproto/IEvaluator.java
@@ -1,24 +1,28 @@
 package uk.bs338.hashLisp.jproto;
 
-import java.util.Arrays;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public interface IEvaluator<V extends IValue> {
     V eval_one(V val);
 
-    default List<V> eval_multi(List<V> vals) {
-        V[] arr = (V[])vals.toArray();
-        eval_multi_inplace(arr);
-        return Arrays.asList(arr);
+    default @NotNull List<V> eval_multi(@NotNull List<V> vals) {
+        ArrayList<V> out = new ArrayList<>(vals.size());
+        for (V val : vals) {
+            out.add(eval_one(val));
+        }
+        return out;
     }
 
-    default void eval_multi_inplace(V[] vals) {
+    default void eval_multi_inplace(@NotNull V[] vals) {
         for (int i = 0; i < vals.length; i++) {
             vals[i] = eval_one(vals[i]);
         }
     }
 
-    V eval_hnf(V val);
+    @NotNull V eval_hnf(@NotNull V val);
 
-    V apply(V head, V args);
+    @NotNull V apply(@NotNull V head, @NotNull V args);
 }
