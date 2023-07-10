afa6999e4ecf415e4751f5099b3aecfec5a964b7
Benjamin Smith <bsmith@cabbage.org.uk>
2023-07-10 20:42:27 +0100

    Add fmap to ConsPair

--- jproto/src/main/java/uk/bs338/hashLisp/jproto/ConsPair.java
+++ jproto/src/main/java/uk/bs338/hashLisp/jproto/ConsPair.java
@@ -1,10 +1,16 @@
 package uk.bs338.hashLisp.jproto;
 
+import java.util.function.Function;
+
 public record ConsPair<V extends IValue> (
     V fst,
     V snd
 ) {
     static public <V extends IValue> ConsPair<V> of(V fst, V snd) {
         return new ConsPair<>(fst, snd);
+    }
+    
+    public <T extends IValue> ConsPair<T> fmap(Function<V, T> func) {
+        return new ConsPair<>(func.apply(fst), func.apply(snd));
     }
 }
