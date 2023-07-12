d2d2c39f735fbcae81ab4783a6c1690550cf8c1a
Benjamin Smith <bsmith@cabbage.org.uk>
2023-07-12 23:13:50 +0100

    Add tests for Context

---
+++ jproto/src/test/java/uk/bs338/hashLisp/jproto/wrapped/ContextTest.java
@@ -0,0 +1,42 @@
+package uk.bs338.hashLisp.jproto.wrapped;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import uk.bs338.hashLisp.jproto.Utilities;
+import uk.bs338.hashLisp.jproto.eval.LazyEvaluator;
+import uk.bs338.hashLisp.jproto.hons.HonsHeap;
+import uk.bs338.hashLisp.jproto.reader.CharClassifier;
+import uk.bs338.hashLisp.jproto.reader.Reader;
+import uk.bs338.hashLisp.jproto.reader.Tokeniser;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContextTest {
+    HonsHeap heap;
+    Reader reader;
+    LazyEvaluator evaluator;
+    Context context;
+
+    @BeforeEach
+    void setUp() {
+        heap = new HonsHeap();
+        reader = new Reader(heap, Tokeniser.getFactory(new CharClassifier()));
+        evaluator = new LazyEvaluator(heap);
+        context = new Context(heap, reader, evaluator);
+    }
+
+    @Test
+    void eval_one() {
+        /* this also exercises using Context as a WrappedHeap/IHeap! */
+        var value = Utilities.makeList(context, context.makeSymbol("add"), context.makeSmallInt(1), context.makeSmallInt(2));
+        var retval = context.eval_one(value);
+        assertEquals(context.makeSmallInt(3), retval);
+    }
+
+    @Test
+    void read() {
+        var readResult = context.read("(1 . 2)");
+        assertTrue(readResult.isSuccess());
+        assertEquals(context.cons(context.makeSmallInt(1), context.makeSmallInt(2)), readResult.getValue());
+    }
+}
